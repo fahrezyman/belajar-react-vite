f031706ce8314f6f84f35f06c08055f17bef2427
fahrezyman <ilhamfahrezyxz@gmail.com>
2023-08-28 17:32:01 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-slate-600 min-h-screen items-center">
+      <h1 className="font-mono">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
